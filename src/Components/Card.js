6f4542b5e0c5179edd9ff6fb567623f5b9eb1c93
jjunseokk <p.junseok53@gmail.com>
2023-03-02 17:54:37 +0900

    test

---
+++ src/Components/Card.js
@@ -0,0 +1,24 @@
+import React from "react";
+import '../Style/join.css'
+import "swiper/css"; //basic
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/effect-cards";
+import 'swiper/css/scrollbar';
+
+
+
+function Card(props) {
+    return (
+        <button onClick={() => { alert("???") }}>
+            <div className="img-frame">
+                <img src={props.img} alt={props.alt} className="slide1" />
+                <div className="text-frame">
+                    <h1>{props.title}</h1>
+                </div>
+            </div>
+        </button>
+    )
+}
+
+export default Card;
