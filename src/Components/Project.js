6a94d2839880b2f428c153cf8d91532e7444732f
박준석 <parkjunseok@bagjunseog-ui-MacBookAir.local>
2023-04-14 13:28:14 +0900

    랜덤숫자게임 프로젝트 url 변경

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -126,7 +126,7 @@ function Project() {
                     img1 ="https://img.shields.io/badge/CSS3-1572B6?style=flat-square&logo=CSS3&logoColor=white"
                     img2 ="https://img.shields.io/badge/HTML5-E34F26?style=flat-square&logo=HTML5&logoColor=white"
                     img3 ="https://img.shields.io/badge/Javascript-F7DF1E?style=flat-square&logo=Javascript&logoColor=white"
-                    page ="https://nimble-tartufo-486be8.netlify.app"
+                    page ="https://rnadomgame.netlify.app"
                     git="https://github.com/jjunseokk/RandomNumber"
                     />
                 </SwiperSlide>
